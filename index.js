11895e87c1cd0f63319b0a2de3961303ad62eb2f
BabriWala <hanzalafarhann@gmail.com>
2022-11-30 12:41:28 +0600

    User Insert Api Done

--- index.js
+++ index.js
@@ -15,7 +15,25 @@ app.use(cors())
 const uri = "mongodb+srv://oldFriends:9iBA3rHgC0ISOhMF@cluster0.o5lz3b5.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        // Database
+        const userCollection = client.db('oldFriends').collection('users');
 
+        // User Insert
+        app.post('/users', async(req,res)=>{
+            const body = req.body;
+            const doc = body;
+            const result = await userCollection.insertOne(doc);
+            res.send(result);
+        })
+
+    }
+    catch{
+
+    }
+}
+run();
 
 
 app.listen(port, ()=>{
